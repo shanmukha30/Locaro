35ce46ebb19a3dc4f257226e3bc9d6338b6cb721
Sathish <shanmukhauppalapati11@gmail.com>
2021-04-29 21:34:04 +0530

    Completed UI of NewList Activity

--- app/src/main/java/com/appsnipp/modernlogin/NewRequestActivity.java
+++ app/src/main/java/com/appsnipp/modernlogin/NewRequestActivity.java
@@ -1,14 +1,22 @@
 package com.appsnipp.modernlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Locale;
 
@@ -19,6 +27,9 @@ public class NewRequestActivity extends AppCompatActivity {
 
     @BindView(R.id.spinner)
     Spinner spinner;
+    EditText editText;
+    Calendar myCalendar;
+    public int a = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +57,53 @@ public class NewRequestActivity extends AppCompatActivity {
         countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the your spinner
         spinner.setAdapter(countryAdapter);
+
+        myCalendar = Calendar.getInstance();
+
+       editText= findViewById(R.id.datepicker);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH, i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+                updateLabel();
+            }
+
+        };
+
+        editText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(NewRequestActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
     public void back(View view){
         startActivity(new Intent(this,BuyersActivity.class));
         overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
         finish();
+    }
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editText.setText(sdf.format(myCalendar.getTime()));
+    }
+    public void rangeClick(View view){
+        a = a+1;
+        if(a%2 == 1){
+            view.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.themeColor)));
+        }
+        else{
+            view.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.lightBlue)));
+        }
+
     }
 }
